51373a8e1a30e1f3535008c05ac09f3d131488c9
Alan Hong <xiupan@gmail.com>
2017-06-28 17:51:30 -0500

    Refactor with SQL Database. Full of fail. Not working. Revisit later.

--- todolist.js
+++ todolist.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
+const models = require('./models');
 
 const app = express();
 
@@ -19,7 +20,23 @@ app.listen(3000, function () {
 });
 
 app.get('/', function(request, response){
-  response.render('index', {todoList: todoList, completedList});
+  models.Todo.findAll({
+    where: {
+      completed: false
+    }
+  }).then(function(todoList){
+    models.Todo.findAll({
+      where: {
+        completed: true
+      }
+    })
+  }).then(function(completed){
+    console.log(todoList);
+    response.render('index', {
+      todoList: todoList,
+      completedList: completed
+    });
+  });
 });
 
 var todoList = [];
@@ -29,20 +46,41 @@ app.post('/', function(request, response){
   var item = request.body.todoAdd;
   var toComplete = request.body.incompleteTask;
 
-  if(item){
-    todoList.push(item);
-    console.log('todoList ' + todoList);
-  } else if (toComplete){
-    for (var i = 0; i < todoList.length; i++) {
-      if(todoList[i] === toComplete){
-        todoList.splice(i,1);
-      }
-    }
-    completedList.push(toComplete);
-    console.log('toComplete ' + toComplete);
-  }
-
-  response.render('index', {
-    todoList: todoList, completedList
+  const itemBuild = models.Todo.build({ // adds the user input to the database
+    item: item,
+    completed: false
   });
+
+  itemBuild.save().then(function (newTodo) { // saves to database
+    todoList.push(newTodo);
+  })
+
+  models.Todo.update({
+    completed: true
+  }, {
+    where: {
+      item: toComplete
+    }
+  }).then(function(completed){
+    console.log(completed);
+    response.redirect('/');
+  })
+
+  // response.render('index', {
+  //   todoList: todoList,
+  //   completedList: completed
+  // });
+
+  // if(item){
+  //   todoList.push(item);
+  //   console.log('todoList ' + todoList);
+  // } else if (toComplete){
+  //   for (var i = 0; i < todoList.length; i++) {
+  //     if(todoList[i] === toComplete){
+  //       todoList.splice(i,1);
+  //     }
+  //   }
+  //   completedList.push(toComplete);
+  //   console.log('toComplete ' + toComplete);
+  // }
 });
